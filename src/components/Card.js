6049a51cd46f136ab974a6daa5ca9926b5f5d3aa
pspanoudakis <pavlos.spanoudakis@gmail.com>
2021-08-07 13:01:23 +0300

    Styling non selected cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,30 @@
 import React, {Component} from 'react';
-//import { cardBackground } from '../CardExports';
+import { cardBackground } from '../CardExports';
 
 export class Card extends Component {
-    getStyle = (isSelected) => {
-        let style = {};
-        if (isSelected) {
-            style.opacity = '1';
-        }
-        else {
-            //style.opacity = '0.6';
-            style.opacity = '1';
-        }
-        Object.assign(style, this.props.style);
-        return style;
-    }
-
     clickDetected = (e) => {
         this.props.clickCallback(this.props.id);
     }
 
     render() {
-        const isSelected = this.props.selected;
+        let imgStyle = {
+            width: '100%',
+            height: '100%'
+        };
+        let divStyle = {
+            opacity: '1'
+        }
+        if (!this.props.selected) {
+            imgStyle.opacity = '0.66';
+            divStyle.backgroundImage = `url(${cardBackground})`;
+            divStyle.backgroundSize = '100% 100%'
+        }
+
+        Object.assign(divStyle, this.props.style);
         return (
-            <div>
+            <div style={divStyle}>
                 <img src={this.props.cardImg} alt={this.props.alt}
-                onClick={this.clickDetected} style={this.getStyle(isSelected)}/>
+                onClick={this.clickDetected} style={imgStyle}/>
             </div>            
         )
     }
