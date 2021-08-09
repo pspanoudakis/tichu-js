c6fb1f52fc5bb3d062d593a6f0c9a7b6fb3be7cb
pspanoudakis <pavlos.spanoudakis@gmail.com>
2021-08-09 22:16:51 +0300

    Majong request selection box

---
+++ src/components/RequestSelectionBox.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import { normalCards } from '../CardInfo';
+
+export class RequestSelectionBox extends Component {
+
+    selectionMade(event) {
+        this.props.requestMade(event.target.id);
+    }
+
+    render() {
+        const selectionBoxStyle = {
+            display: 'flex',
+            width: '100%',
+            height: 'min-content',
+            overflow: 'visible'
+        };
+        const selectionButtonStyle = {
+            overflow: 'visible'
+        }
+        return (
+        <div style={selectionBoxStyle}>
+            {Object.keys(normalCards).reverse().map(name => 
+                <button key={name} id={name} onClick={this.selectionMade}>
+                {name}
+                </button>
+            )}
+        </div>
+        );
+    }
+}
