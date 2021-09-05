3175844fb0364f7219eec3cf3becea9bac81fb52
pspanoudakis <pavlos.spanoudakis@gmail.com>
2021-09-05 03:39:29 +0300

    Game Logic routines done

--- src/CardCombinations.js
+++ src/CardCombinations.js
@@ -1,4 +1,11 @@
-import { normalCardKeys, normalCards } from "./CardInfo";
+import { cardColors, normalCardKeys, normalCards, specialCards } from "./CardInfo";
+
+const letterValues = {
+    J: 11,
+    Q: 12,
+    K: 13,
+    A: 14
+}
 
 function isBetween(target, a, b) {
     return target >= a && target <= b;
@@ -10,7 +17,8 @@ export const cardCombinations = {
     STEPS: 'Steps',
     TRIPLET: 'Triplet',
     COUPLE: 'Couple',
-    SINGLE: 'Single'
+    SINGLE: 'Single',
+    BOMB: 'Bomb'
 }
 
 export class CardCombination {
@@ -361,7 +369,6 @@ export class FullHouse extends CardCombination {
     static getStrongestRequested(cards, requestedCard) {
         if (cards.length >= 5) {
             let phoenixUsed = true;
-            let nonRequestedRequired = 0;
             let requestedOccurences = 0;
             // Counts occurences of each card
             let cardOccurences = new Map();
@@ -387,14 +394,12 @@ export class FullHouse extends CardCombination {
                     // Using phoenix to replace a second requested card
                     phoenixUsed = true;
                     // Looking for the strongest card with 3 occurences now...
-                    nonRequestedRequired = 3;
                     requestedOccurences = 2;
                 }
                 else { return null; }
             }
             else {
                 requestedOccurences = Math.min(requestedOccurences, 3);
-                nonRequestedRequired = 5 - requestedOccurences;
             }
             return strongestRequestedFullHouse(
                 cardOccurences, requestedCard, requestedOccurences, phoenixUsed
@@ -439,4 +444,127 @@ function strongestRequestedFullHouse(cardOccurences, requestedCard, requestedOcc
         }
     }
     return null;
+}
+
+export class Bomb extends CardCombination{
+    constructor(upperCard, lowerCard, color='') {
+        const topValue = (letterValues[upperCard] !== undefined) ? letterValues[upperCard] : parseInt(upperCard);
+        const lower = (letterValues[lowerCard] !== undefined) ? letterValues[lowerCard] : parseInt(lowerCard);
+        const length = topValue - lower + 1;
+        super(cardCombinations.BOMB, length > 1 ? length : 4, topValue);
+        this.color = color;
+    }
+
+    static createBomb(cards) {
+        if (cards.length > 4) {
+            // Kenta bomb
+            let previousCardValue = cards[0].value;
+            const color = cards[0].color;
+            for (let i = 1; i < cards.length; i++) {
+                if (cards[i].value !== previousCardValue - 1 || cards[i].color !== color) {
+                    return null;
+                }
+                previousCardValue--;
+            }
+            return new Bomb(cards[0].name, cards[cards.length - 1].name, color);
+        }
+        else if (cards.length === 4) {
+            // 4 same cards bomb
+            const name = cards[0].name;
+            if(cards.slice(1, 4).some(card => card.name !== name))
+            {
+                return null;
+            }
+            return new Bomb(name, name);
+        }
+        else {
+            // invalid
+            return null;
+        }
+    }
+
+    static compareBombs(a, b) {
+        if (a === b) { return 0; }
+        if (a === null) { return -1; }
+        if (b === null) { return 1; }
+
+        if (a.length !== b.length) {
+            return a.length - b.length;
+        }
+        if (a.upper !== b.upper) {
+            return a.upper - b.upper;
+        }
+        return 0;
+    }
+
+    static getStrongestBomb(cards) {
+        let strongestBomb = null;
+
+        let groupedNormalCards = {};
+        for (const key of normalCardKeys) {
+            groupedNormalCards[key] = {};
+            for (const color of Object.values(cardColors)) {
+                groupedNormalCards[key][color] = false;
+            }
+        }
+        for (const card of cards) {
+            if(!Object.values(specialCards).includes(card.name)) {
+                // not a special card
+                groupedNormalCards[card.name][card.color] = true;
+            }
+        }
+        const cardGroupArray = Object.entries(groupedNormalCards);
+
+        for (const color of Object.values(cardColors)) {
+            let upperIndex, lowerIndex;
+            let lengthCounter = 0;
+            let colorStrongest = null;
+            for (let i = 0; i < cardGroupArray.length; i++) {
+                const [,colors] = cardGroupArray[i];
+                if (colors[color] === false) {
+                    if (lengthCounter >= 5) {
+                        const [upperName] = cardGroupArray[upperIndex];
+                        const [lowerName] = cardGroupArray[lowerIndex];
+                        let bomb = new Bomb(upperName, lowerName, color);
+                        if (Bomb.compareBombs(colorStrongest, bomb) < 0) {
+                            colorStrongest = bomb;
+                        }
+                    }
+                    lengthCounter = 0;
+                }
+                else {
+                    if (lengthCounter === 0) {
+                        lowerIndex = i;
+                    }
+                    upperIndex = i;
+                    lengthCounter++;
+                }
+            }
+            if (lengthCounter >= 5) {
+                const [upperName] = cardGroupArray[upperIndex];
+                const [lowerName] = cardGroupArray[lowerIndex];
+                let bomb = new Bomb(upperName, lowerName, color);
+                if (Bomb.compareBombs(colorStrongest, bomb) < 0) {
+                    colorStrongest = bomb;
+                }
+            }
+            if (Bomb.compareBombs(strongestBomb, colorStrongest) < 0) {
+                strongestBomb = colorStrongest;
+            }
+        }
+        if (strongestBomb === null) {
+            for (let i = cardGroupArray.length - 1; i >= 0; i--) {
+                const [name, colors] = cardGroupArray[i];
+                if (!Object.values(colors).includes(false)) {
+                    // we have a bomb
+                    let bomb = new Bomb(name, name);
+                    // We iterate over the cards starting from the strongest,
+                    // so we won't find a stronger bomb
+                    strongestBomb = bomb;
+                    break;
+                }
+            }
+        }
+        return strongestBomb;
+    }
 }
--- src/GameLogic.js
+++ src/GameLogic.js
@@ -1,4 +1,3 @@
-import { BombInfo } from "./BombInfo";
 import { specialCards } from "./CardInfo";
 import { playerKeys } from "./components/Gameboard";
 import { cardCombinations,
@@ -8,7 +7,8 @@ import { cardCombinations,
          Triplet,
          FullHouse,
          Steps,
-         Kenta } from "./CardCombinations";
+         Kenta,
+         Bomb } from "./CardCombinations";
 
 export class GameLogic {
 
@@ -24,26 +24,192 @@ export class GameLogic {
         return false;
     }
 
-    static isPlayable(tableCards, selectedCards, message) {
-        if (tableCards.length > 0) {
-            // Check if playable
-        }
-        else {
-            // Check if combination is valid
+    static isPlayable(tableCombination, selectedCombination) {
+        if (tableCombination !== undefined) {
+            if (selectedCombination.combination === cardCombinations.BOMB) {
+                if (tableCombination.combination === cardCombinations.BOMB) {
+                    return Bomb.compareBombs(selectedCombination, tableCombination) > 0;
+                }
+                return true;
+            }
+            if (selectedCombination.combination === tableCombination.combination) {
+                return tableCombination.compare(selectedCombination) < 0;
+            }
+            return false;
         }
         return true;
     }
 
+    static canPassTurn(tableCombination, requestedCard, playerCards) {
+        if (tableCombination !== undefined) {
+            switch(tableCombination.combination) {
+                case cardCombinations.BOMB:
+                    return true;
+                case cardCombinations.SINGLE:
+                    if (tableCombination.compare( tableCombination,
+                        SingleCard.getStrongestRequested(playerCards, requestedCard) ) < 0) {
+                            return false;
+                    }
+                    break;
+                case cardCombinations.COUPLE:
+                    if (tableCombination.compare( tableCombination,
+                        CardCouple.getStrongestRequested(playerCards, requestedCard) ) < 0) {
+                            return false;
+                    }
+                    break;
+                case cardCombinations.TRIPLET:
+                    if (tableCombination.compare( tableCombination,
+                        Triplet.getStrongestRequested(playerCards, requestedCard) ) < 0) {
+                            return false;
+                    }
+                    break;
+                case cardCombinations.FULLHOUSE:
+                    if (tableCombination.compare( tableCombination,
+                        FullHouse.getStrongestRequested(playerCards, requestedCard) ) < 0) {
+                            return false;
+                    }
+                    break;
+                case cardCombinations.STEPS:
+                    if (tableCombination.compare( tableCombination,
+                        Steps.getStrongestRequested(playerCards, requestedCard,
+                        tableCombination.length) ) < 0) {
+                            return false;
+                    }
+                    break;
+                case cardCombinations.KENTA:
+                    if (tableCombination.compare( tableCombination,
+                        Kenta.getStrongestRequested(playerCards, requestedCard,
+                        tableCombination.length) ) < 0) {
+                            return false;
+                    }
+                    break;
+                default:
+                    return false;
+                
+                // TODO: If the player has a bomb with the requested cards, he can't pass
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    static createCombination(cards) {
+        let combination = null;
+        switch (cards.length) {
+            case 1:
+                combination = SingleCard.create(cards);
+                break;
+            case 2:
+                combination = CardCouple.create(cards);
+                break;
+            case 3:
+                combination = Triplet.create(cards);
+                break;
+            default:
+                combination = Bomb.createBomb(cards);
+                if (combination === null) {
+                    if (cards.length === 5) {
+                        combination = FullHouse.create(cards);
+                    }
+                    else if (cards.length % 2 === 0) {
+                        combination = Steps.create(cards);
+                    }
+                    if (combination !== null) { return combination; }
+                    combination = Kenta.create(cards);
+                }
+                break;
+        }
+        return combination;
+    }
+
+    static isMajongCompliant(requestedCard, tableCombination, allCards, combination, selectedCards) {
+        if (tableCombination === undefined) {
+            // See if there is *any* valid combination with the requested card
+            if (SingleCard.getStrongestRequested(allCards, requestedCard) !== null) {
+                if (SingleCard.getStrongestRequested(selectedCards, requestedCard) !== null) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        else {
+            switch(tableCombination.combination) {
+                case cardCombinations.BOMB:
+                    return true;
+                case cardCombinations.SINGLE:
+                    if (tableCombination.compare( tableCombination,
+                        SingleCard.getStrongestRequested(allCards, requestedCard) ) < 0) {
+                            if ( (combination.combination !== cardCombinations.SINGLE) ||
+                                tableCombination.compare(combination) >= 0) {
+                                    return false;
+                                }
+                    }
+                    break;
+                case cardCombinations.COUPLE:
+                    if (tableCombination.compare( tableCombination,
+                        CardCouple.getStrongestRequested(allCards, requestedCard) ) < 0) {
+                            if ( (combination.combination !== cardCombinations.COUPLE) ||
+                                tableCombination.compare(combination) >= 0) {
+                                    return false;
+                                }
+                    }
+                    break;
+                case cardCombinations.TRIPLET:
+                    if (tableCombination.compare( tableCombination,
+                        Triplet.getStrongestRequested(allCards, requestedCard) ) < 0) {
+                            if ( (combination.combination !== cardCombinations.TRIPLET) ||
+                                tableCombination.compare(combination) >= 0) {
+                                    return false;
+                                }
+                    }
+                    break;
+                case cardCombinations.FULLHOUSE:
+                    if (tableCombination.compare( tableCombination,
+                        FullHouse.getStrongestRequested(allCards, requestedCard) ) < 0) {
+                            if ( (combination.combination !== cardCombinations.FULLHOUSE) ||
+                                tableCombination.compare(combination) >= 0) {
+                                    return false;
+                                }
+                    }
+                    break;
+                case cardCombinations.STEPS:
+                    if (tableCombination.compare( tableCombination,
+                        Steps.getStrongestRequested(allCards, requestedCard,
+                        tableCombination.length) ) < 0) {
+                            if ( (combination.combination !== cardCombinations.STEPS) ||
+                                tableCombination.compare(combination) >= 0) {
+                                    return false;
+                                }
+                    }
+                    break;
+                case cardCombinations.KENTA:
+                    if (tableCombination.compare( tableCombination,
+                        Kenta.getStrongestRequested(allCards, requestedCard,
+                        tableCombination.length) ) < 0) {
+                            if ( (combination.combination !== cardCombinations.KENTA) ||
+                                tableCombination.compare(combination) >= 0) {
+                                    return false;
+                                }
+                    }
+                    break;
+                default:
+                    return false;
+                
+                // TODO: If the player has a bomb with the requested cards, he can't pass
+            }
+            return true;
+        }
+    }
+
     static playCards(gameboard, playerKey) {
         let normalCheck = true;
-        let checkBomb = false;
         let selectedCards = [];
+        let allPlayerCards = [];
         let playerHands = {};
         let requestedCard = gameboard.state.table.requestedCardName;
         let nextPlayerIndex = (gameboard.state.currentPlayerIndex + 1) % 4;
-        let message = {
-            text: ''
-        };
         for (const key of playerKeys) {
             if (playerKey !== key) {
                 playerHands[key] = gameboard.state.playerHands[key];
@@ -57,97 +223,105 @@ export class GameLogic {
                     else {
                         playerHands[playerKey].push(card);
                     }
+                    allPlayerCards.push(card);
                 }
             }
         }
-        
-        if (gameboard.state.pendingMajongRequest !== '') {
-            // If there is a pending majong request, the player must play the Majong
-            if (!selectedCards.some(card => card.name === specialCards.MAJONG)) {
-                window.alert("The Majong must be played after a Majong request");
-                return;
+        let combination = GameLogic.createCombination(selectedCards);
+        if (combination !== null) {
+            if (gameboard.state.pendingMajongRequest !== '') {
+                // If there is a pending majong request, the player must play the Majong
+                if (!selectedCards.some(card => card.name === specialCards.MAJONG)) {
+                    window.alert("The Majong must be played after a Majong request");
+                    return;
+                }
+                else {
+                    requestedCard = gameboard.state.pendingMajongRequest;
+                }
             }
-            else {
-                requestedCard = gameboard.state.pendingMajongRequest;
+            else if (gameboard.state.pendingBombToBePlayed) {
+                if (combination.combination !== cardCombinations.BOMB) {
+                    window.alert("A bomb must be played");
+                    return;
+                }
+                else {
+                    const tableCombination = gameboard.state.table.combination;
+                    if ( tableCombination.combination === cardCombinations.BOMB) {
+                        if (Bomb.compareBombs(tableCombination, combination) >= 0) {
+                            window.alert("The selected combination cannot be played");
+                            return;
+                        }
+                    }
+                    
+                }
+                normalCheck = false;
             }
-        }
-        /* gameboard will be enabled in the future        
-        else if (gameboard.state.table.requestedCardName !== '') {
-            // TODO: If there is an active majong request, the player must play the requested
-            // card if present AND playable
-            debugger;
-            return;
-        }
-        */
-        // TODO: check 1) is combination valid 2) is it playable?
-        else if (gameboard.state.pendingBombToBePlayed) {
-            let playerBomb = BombInfo.createBomb(selectedCards)
-            if ( playerBomb === null) {
-                window.alert("A bomb must be played");
-                return;
-            }
-            else {
-                let tableBomb = BombInfo.createBomb(gameboard.state.table.currentCards);
-                if (BombInfo.compareBombs(tableBomb, playerBomb) >= 0) {
-                    window.alert("The selected combination cannot be played");
+            else if (gameboard.state.table.requestedCardName !== '') {
+                if ( !GameLogic.isMajongCompliant(gameboard.state.table.requestedCardName,
+                     gameboard.state.table.combination, allPlayerCards, combination, selectedCards)) {
+                    window.alert("A combination which contains the requested card is required.");
                     return;
                 }
             }
-            normalCheck = false;
-        }
-        else if (selectedCards.some(card => card.name === specialCards.DOGS)) {
-            if (selectedCards.length !== 1) {
-                window.alert('Invalid card combination');
-                return;
+            else if (selectedCards.some(card => card.name === specialCards.DOGS)) {
+                normalCheck = false;
+                nextPlayerIndex = (gameboard.state.currentPlayerIndex + 2) % 4;
+                selectedCards = [];
+                combination = undefined;
             }
-            normalCheck = false;
-            nextPlayerIndex = (gameboard.state.currentPlayerIndex + 2) % 4;
-            selectedCards = [];
-        }
-        if (normalCheck) {
-            if ( !GameLogic.isPlayable(gameboard.state.table.currentCards,
-                selectedCards, message) ) {
-                    window.alert(message.text);
+            if (normalCheck) {
+                if ( !GameLogic.isPlayable(gameboard.state.table.combination, combination) ) {
+                    window.alert("This combination cannot be played");
                     return;
+                }
             }
-        }
-        while (gameboard.state.playerHands[playerKeys[nextPlayerIndex]].length === 0) {
-            nextPlayerIndex = (nextPlayerIndex + 1) % 4;
-        }
-        gameboard.setState({
-            playerHands: playerHands,
-            currentPlayerIndex: nextPlayerIndex,
-            pendingMajongRequest: '',
-            pendingDragonToBeGiven: false,
-            pendingBombToBePlayed: false,
-            table: {
-                previousCards: gameboard.state.table.previousCards.concat(gameboard.state.table.currentCards),
-                currentCards: selectedCards,
-                currentCardsOwnerIndex: gameboard.state.currentPlayerIndex,
-                requestedCardName: requestedCard
+            while (gameboard.state.playerHands[playerKeys[nextPlayerIndex]].length === 0) {
+                nextPlayerIndex = (nextPlayerIndex + 1) % 4;
             }
-        })
+            gameboard.setState({
+                playerHands: playerHands,
+                currentPlayerIndex: nextPlayerIndex,
+                pendingMajongRequest: '',
+                pendingDragonToBeGiven: false,
+                pendingBombToBePlayed: false,
+                table: {
+                    previousCards: gameboard.state.table.previousCards.concat(gameboard.state.table.currentCards),
+                    currentCards: selectedCards,
+                    combination: combination,
+                    currentCardsOwnerIndex: gameboard.state.currentPlayerIndex,
+                    requestedCardName: requestedCard
+                }
+            });
+        }
+        else {
+            window.alert('Invalid card combination');
+        }
     }
 
     static passTurn(gameboard) {
-        let nextPlayerIndex = (gameboard.state.currentPlayerIndex + 1) % 4;
-        let newState = {};
-        let endedRound = false;
-        while (gameboard.state.playerHands[playerKeys[nextPlayerIndex]].length === 0) {
+        if (GameLogic.canPassTurn()) {
+            let nextPlayerIndex = (gameboard.state.currentPlayerIndex + 1) % 4;
+            let newState = {};
+            let endedRound = false;
+            while (gameboard.state.playerHands[playerKeys[nextPlayerIndex]].length === 0) {
+                if (nextPlayerIndex === gameboard.state.table.currentCardsOwnerIndex) {
+                    GameLogic.endRound(gameboard, newState, nextPlayerIndex);
+                    endedRound = true;
+                }
+                nextPlayerIndex = (nextPlayerIndex + 1) % 4;
+            }
             if (nextPlayerIndex === gameboard.state.table.currentCardsOwnerIndex) {
                 GameLogic.endRound(gameboard, newState, nextPlayerIndex);
-                endedRound = true;
+                if (endedRound) {
+                    console.log('End round twice bug');
+                }
             }
-            nextPlayerIndex = (nextPlayerIndex + 1) % 4;
+            newState.currentPlayerIndex = nextPlayerIndex;
+            gameboard.setState(newState);
         }
-        if (nextPlayerIndex === gameboard.state.table.currentCardsOwnerIndex) {
-            GameLogic.endRound(gameboard, newState, nextPlayerIndex);
-            if (endedRound) {
-                console.log('End round twice bug');
-            }
-        }
-        newState.currentPlayerIndex = nextPlayerIndex;
-        gameboard.setState(newState);
+        else {
+            window.alert("A combination which contains the requested card is required.");
+        }        
     }
 
     static endRound(gameboard, newState, cardsOwnerIndex) {
@@ -166,6 +340,7 @@ export class GameLogic {
                 newState.table = {};
                 newState.table.previousCards = [];
                 newState.table.currentCards = [];
+                newState.table.combination = undefined;
                 newState.table.currentCardsOwnerIndex = -1;
                 newState.table.requestedCardName = gameboard.state.table.requestedCardName;
                 console.log(newState.playerHeaps[playerKeys[i]]);
