82719689fb654625a8cd1024f44254e865db5947
pspanoudakis <pavlos.spanoudakis@gmail.com>
2024-10-21 19:54:14 +0300

    Externalize app context mutation logic

--- src/AppContext.ts
+++ src/AppContext.ts
@@ -1,6 +1,8 @@
 import { createContext } from 'react'
 import { createInitialGameState, GameState } from './state_types/GameState'
 import { Socket } from 'socket.io-client'
+import { GameRoundStartedEvent, PlayerJoinedEvent, WaitingForJoinEvent } from './game_logic/shared/ServerEvents';
+import { PLAYER_KEYS, PlayerBet } from './game_logic/shared/shared';
 
 export type AppContextState = {
     gameContext: GameState,
@@ -32,3 +34,145 @@ export const setSocket = (ctx: AppContextType, socket: Socket) =>
         gameContext: ctxState.gameContext,
         socket,
     }));
+
+const getTeammateIdx =
+    (playerIdx: number) => (playerIdx + 2) % PLAYER_KEYS.length;
+const getLeftOpponentIdx =
+    (playerIdx: number) => (playerIdx + PLAYER_KEYS.length - 1) % PLAYER_KEYS.length;
+const getRightOpponentIdx =
+    (playerIdx: number) => (playerIdx + 1) % PLAYER_KEYS.length;
+
+export function handleWaitingForJoinEvent(
+    ctx: AppContextState, e: WaitingForJoinEvent
+): AppContextState {
+    const thisIdx = PLAYER_KEYS.indexOf(e.playerKey);
+    const teammateIdx = getTeammateIdx(thisIdx);
+    const leftOpIdx = getLeftOpponentIdx(thisIdx);
+    const rightOpIdx = getRightOpponentIdx(thisIdx);
+    return {
+        ...ctx,
+        gameContext: {
+            ...ctx.gameContext,
+            thisPlayer: {
+                playerKey: e.playerKey,
+                playerIndex: thisIdx,
+                nickname: '',
+            },
+            teammate: (
+                e.data.presentPlayers[PLAYER_KEYS[teammateIdx]] ? {
+                    playerKey: PLAYER_KEYS[teammateIdx],
+                    playerIndex: teammateIdx,
+                    nickname: e.data.presentPlayers[
+                        PLAYER_KEYS[teammateIdx]
+                    ] ?? '',
+                } : ctx.gameContext.teammate
+            ),
+            leftOpponent: (
+                e.data.presentPlayers[PLAYER_KEYS[leftOpIdx]] ? {
+                    playerKey: PLAYER_KEYS[leftOpIdx],
+                    playerIndex: leftOpIdx,
+                    nickname: e.data.presentPlayers[
+                        PLAYER_KEYS[leftOpIdx]
+                    ] ?? '',
+                } : ctx.gameContext.leftOpponent
+            ),
+            rightOpponent: (
+                e.data.presentPlayers[PLAYER_KEYS[rightOpIdx]] ? {
+                    playerKey: PLAYER_KEYS[rightOpIdx],
+                    playerIndex: rightOpIdx,
+                    nickname: e.data.presentPlayers[
+                        PLAYER_KEYS[rightOpIdx]
+                    ] ?? '',
+                } : ctx.gameContext.rightOpponent
+            ),
+        }
+    };
+}
+
+export function handlePlayerJoinedEvent(
+    ctx: AppContextState, e: PlayerJoinedEvent
+): AppContextState {
+    const thisIdx = ctx.gameContext.thisPlayer?.playerIndex;
+    if (thisIdx === undefined) {
+        throw new Error('Another player joined before client player index received.');
+    }
+    const teammateIdx = getTeammateIdx(thisIdx);
+    const leftOpIdx = getLeftOpponentIdx(thisIdx);
+    const rightOpIdx = getRightOpponentIdx(thisIdx);
+    return {
+        ...ctx,
+        gameContext: {
+            ...ctx.gameContext,
+            thisPlayer: e.playerKey === PLAYER_KEYS[thisIdx] ? {
+                playerKey: e.playerKey,
+                playerIndex: PLAYER_KEYS.indexOf(e.playerKey),
+                nickname: e.data.playerNickname,
+            } : ctx.gameContext.thisPlayer,
+            leftOpponent: e.playerKey === PLAYER_KEYS[leftOpIdx] ? {
+                playerKey: e.playerKey,
+                playerIndex: PLAYER_KEYS.indexOf(e.playerKey),
+                nickname: e.data.playerNickname,
+            } : ctx.gameContext.leftOpponent,
+            rightOpponent: e.playerKey === PLAYER_KEYS[rightOpIdx] ? {
+                playerKey: e.playerKey,
+                playerIndex: PLAYER_KEYS.indexOf(e.playerKey),
+                nickname: e.data.playerNickname,
+            } : ctx.gameContext.rightOpponent,
+            teammate: e.playerKey === PLAYER_KEYS[teammateIdx] ? {
+                playerKey: e.playerKey,
+                playerIndex: PLAYER_KEYS.indexOf(e.playerKey),
+                nickname: e.data.playerNickname,
+            } : ctx.gameContext.teammate,
+        }
+    }
+}
+
+export function handleGameRoundStartedEvent(
+    ctx: AppContextState, e: GameRoundStartedEvent
+): AppContextState {
+    if (
+        !ctx.gameContext.thisPlayer ||
+        !ctx.gameContext.leftOpponent ||
+        !ctx.gameContext.rightOpponent ||
+        !ctx.gameContext.teammate
+    ) {
+        console.error(`Player state not initialized: `, ctx.gameContext);
+        throw new Error(`Player state not initialized.`);
+    }
+    return {
+        ...ctx,
+        gameContext: {
+            ...ctx.gameContext,
+            currentRoundState: {
+                thisPlayer: {
+                    cardKeys: e.data.partialCards,
+                    pendingBomb: false,
+                    playerBet: PlayerBet.NONE,
+                    playerKey: ctx.gameContext.thisPlayer.playerKey,
+                },
+                tableState: {
+                    pendingDragonSelection: false,
+                    currentCardKeys: [],
+                },
+                leftOpponent: {
+                    numberOfCards: e.data.partialCards.length,
+                    pendingBomb: false,
+                    playerBet: PlayerBet.NONE,
+                    playerKey: ctx.gameContext.leftOpponent.playerKey,
+                },
+                rightOpponent: {
+                    numberOfCards: e.data.partialCards.length,
+                    pendingBomb: false,
+                    playerBet: PlayerBet.NONE,
+                    playerKey: ctx.gameContext.rightOpponent.playerKey,
+                },
+                teammate: {
+                    numberOfCards: e.data.partialCards.length,
+                    pendingBomb: false,
+                    playerBet: PlayerBet.NONE,
+                    playerKey: ctx.gameContext.teammate.playerKey,
+                },
+            }
+        }
+    }
+}
--- src/components/GameSession.tsx
+++ src/components/GameSession.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { createSessionSocketURI } from "../API/coreAPI";
-import { ServerEventType, zGameRoundStartedEvent, zPlayerJoinedEvent, zWaitingForJoinEvent } from "../game_logic/shared/ServerEvents";
-import { createInitialGameState } from "../state_types/GameState";
-import { PLAYER_KEYS, PlayerBet } from "../game_logic/shared/shared";
+import {
+    ServerEventType,
+    zGameRoundStartedEvent,
+    zPlayerJoinedEvent,
+    zWaitingForJoinEvent
+} from "../game_logic/shared/ServerEvents";
 import { Scoreboard } from "./Scoreboard";
 
 import styles from "../styles/Components.module.css"
-import { AppContext, appContextInitState, AppContextState } from "../AppContext";
+import {
+    AppContext,
+    appContextInitState,
+    handleGameRoundStartedEvent,
+    handlePlayerJoinedEvent,
+    handleWaitingForJoinEvent
+} from "../AppContext";
 import { ClientEventType } from "../game_logic/shared/ClientEvents";
 import { HiddenPlayerHand } from "./HiddenPlayerHand";
 import { ControlledPlayerHand } from "./ControlledPlayerHand";
@@ -22,9 +31,9 @@ function logError(msg?: any, ...optionals: any[]) {
     alert(`${msg}. See console.`);
 }
 
-function eventHandlerWrapper<SE>(
-    validator: (e: any) => SE,
-    eventHandler: (e: SE) => void,
+function eventHandlerWrapper<EventType>(
+    validator: (e: any) => EventType,
+    eventHandler: (e: EventType) => void,
 ) {
     return (event: any) => {
         let e;
@@ -56,49 +65,10 @@ export const GameSession: React.FC<GameSessionProps> = (props) => {
         })
         // Register listeners
         .on('connect', () => {
-            
+            console.log(`SocketIO connection established. Socket ID: ${socket.id}`);
         }).on(ServerEventType.WAITING_4_JOIN, eventHandlerWrapper(
             zWaitingForJoinEvent.parse, e => {
-                setAppContextState(ctx => {
-                    const thisIdx = PLAYER_KEYS.indexOf(e.playerKey);
-                    const teammateIdx = (thisIdx + 2) % PLAYER_KEYS.length;
-                    const leftOpIdx = (thisIdx + PLAYER_KEYS.length - 1) % PLAYER_KEYS.length;
-                    const rightOpIdx = (thisIdx + 1) % PLAYER_KEYS.length;
-                    const newCtx: AppContextState = {
-                        ...ctx,
-                        gameContext: {
-                            ...ctx.gameContext,
-                            thisPlayer: {
-                                playerKey: e.playerKey,
-                                playerIndex: thisIdx,
-                                nickname: '',
-                            },
-                            teammate: (
-                                e.data.presentPlayers[PLAYER_KEYS[teammateIdx]] ? {
-                                    playerKey: PLAYER_KEYS[teammateIdx],
-                                    playerIndex: teammateIdx,
-                                    nickname: e.data.presentPlayers[PLAYER_KEYS[teammateIdx]] ?? '',
-                                } : ctx.gameContext.teammate
-                            ),
-                            leftOpponent: (
-                                e.data.presentPlayers[PLAYER_KEYS[leftOpIdx]] ? {
-                                    playerKey: PLAYER_KEYS[leftOpIdx],
-                                    playerIndex: leftOpIdx,
-                                    nickname: e.data.presentPlayers[PLAYER_KEYS[leftOpIdx]] ?? '',
-                                } : ctx.gameContext.leftOpponent
-                            ),
-                            rightOpponent: (
-                                e.data.presentPlayers[PLAYER_KEYS[rightOpIdx]] ? {
-                                    playerKey: PLAYER_KEYS[rightOpIdx],
-                                    playerIndex: rightOpIdx,
-                                    nickname: e.data.presentPlayers[PLAYER_KEYS[rightOpIdx]] ?? '',
-                                } : ctx.gameContext.rightOpponent
-                            ),
-                        }
-                    };
-                    return newCtx;
-                });
-
+                setAppContextState(ctx => handleWaitingForJoinEvent(ctx, e));
                 socket.emit(
                     ClientEventType.JOIN_GAME, {
                         data: {
@@ -110,94 +80,13 @@ export const GameSession: React.FC<GameSessionProps> = (props) => {
             }
         )).on(ServerEventType.PLAYER_JOINED, eventHandlerWrapper(
             zPlayerJoinedEvent.parse, e => {
-                setAppContextState(ctx => {
-                    const thisIdx = ctx.gameContext.thisPlayer?.playerIndex;
-                    if (thisIdx === undefined) {
-                        throw new Error('Another player joined before client player index received.');
-                    }
-                    const teammateIdx = (thisIdx + 2) % PLAYER_KEYS.length;
-                    const leftOpIdx = (thisIdx + PLAYER_KEYS.length - 1) % PLAYER_KEYS.length;
-                    const rightOpIdx = (thisIdx + 1) % PLAYER_KEYS.length;
-                    const newCtx: AppContextState = {
-                        ...ctx,
-                        gameContext: {
-                            ...ctx.gameContext,
-                            thisPlayer: e.playerKey === PLAYER_KEYS[thisIdx] ? {
-                                playerKey: e.playerKey,
-                                playerIndex: PLAYER_KEYS.indexOf(e.playerKey),
-                                nickname: e.data.playerNickname,
-                            } : ctx.gameContext.thisPlayer,
-                            leftOpponent: e.playerKey === PLAYER_KEYS[leftOpIdx] ? {
-                                playerKey: e.playerKey,
-                                playerIndex: PLAYER_KEYS.indexOf(e.playerKey),
-                                nickname: e.data.playerNickname,
-                            } : ctx.gameContext.leftOpponent,
-                            rightOpponent: e.playerKey === PLAYER_KEYS[rightOpIdx] ? {
-                                playerKey: e.playerKey,
-                                playerIndex: PLAYER_KEYS.indexOf(e.playerKey),
-                                nickname: e.data.playerNickname,
-                            } : ctx.gameContext.rightOpponent,
-                            teammate: e.playerKey === PLAYER_KEYS[teammateIdx] ? {
-                                playerKey: e.playerKey,
-                                playerIndex: PLAYER_KEYS.indexOf(e.playerKey),
-                                nickname: e.data.playerNickname,
-                            } : ctx.gameContext.teammate,
-                        }
-                    }
-                    return newCtx;
-                });
+                setAppContextState(ctx => handlePlayerJoinedEvent(ctx, e));
             }
         )).on(ServerEventType.GAME_ROUND_STARTED, eventHandlerWrapper(
             zGameRoundStartedEvent.parse, e => {
-                setAppContextState(ctx => {
-                    if (
-                        !ctx.gameContext.thisPlayer ||
-                        !ctx.gameContext.leftOpponent ||
-                        !ctx.gameContext.rightOpponent ||
-                        !ctx.gameContext.teammate
-                    ) {
-                        console.error(`Player state not initialized: `, ctx.gameContext);
-                        throw new Error(`Player state not initialized.`);
-                    }
-                    return {
-                        ...ctx,
-                        gameContext: {
-                            ...ctx.gameContext,
-                            currentRoundState: {
-                                thisPlayer: {
-                                    cardKeys: e.data.partialCards,
-                                    pendingBomb: false,
-                                    playerBet: PlayerBet.NONE,
-                                    playerKey: ctx.gameContext.thisPlayer.playerKey,
-                                },
-                                tableState: {
-                                    pendingDragonSelection: false,
-                                    currentCardKeys: [],
-                                },
-                                leftOpponent: {
-                                    numberOfCards: e.data.partialCards.length,
-                                    pendingBomb: false,
-                                    playerBet: PlayerBet.NONE,
-                                    playerKey: ctx.gameContext.leftOpponent.playerKey,
-                                },
-                                rightOpponent: {
-                                    numberOfCards: e.data.partialCards.length,
-                                    pendingBomb: false,
-                                    playerBet: PlayerBet.NONE,
-                                    playerKey: ctx.gameContext.rightOpponent.playerKey,
-                                },
-                                teammate: {
-                                    numberOfCards: e.data.partialCards.length,
-                                    pendingBomb: false,
-                                    playerBet: PlayerBet.NONE,
-                                    playerKey: ctx.gameContext.teammate.playerKey,
-                                },
-                            }
-                        }
-                    }
-                })
+                setAppContextState(ctx => handleGameRoundStartedEvent(ctx, e))
             }
-        ))
+        ));
 
         setAppContextState(ctx => ({
             ...ctx,
