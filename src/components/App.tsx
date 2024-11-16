610d834acef586fdf459461efebbc68ae32b6de3
pspanoudakis <pavlos.spanoudakis@gmail.com>
2024-11-16 10:41:33 +0200

    Remove root wrapper component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ import { WinScoreSelector } from "./WinScoreSelector";
 import styles from "../styles/Components.module.css";
 import tichuLogo from "../res/tichu_logo.png"
 
-export const AppRoot: React.FC<{}> = (props) => {
+const App: React.FC<{}> = (props) => {
 
     const [loading, setLoading] = useState(false);
     const [currentSessionId, setCurrentSessionId] = useState<string | undefined>();
@@ -45,49 +45,53 @@ export const AppRoot: React.FC<{}> = (props) => {
         (s: number) => setWinningScore(s), []
     );
 
-    return <>{
-        loading ?
-        <div>Loading...</div>
-        : (
-            currentSessionId ?
-            <GameSession
-                sessionId={currentSessionId}
-                playerNickname={nickname}
-            />
-            :
-            <div className={styles.enteringSceneContainer}>
-                <img src={tichuLogo} alt={"Tichu Logo"} className={styles.gameLogo}/>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    columnGap: '1ch',
-                }}>
-                    <span>Nickname:</span>
-                    <input
-                        value={nickname}
-                        onChange={e => setNickname(e.target.value)}
-                    />
-                </div>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'start',
-                    rowGap: '1ch',
-                }}>
-                    <WinScoreSelector onSelected={onWinningScoreSelected}/>
+    return (
+        <div className="App-body">{
+            loading ?
+            <div>Loading...</div>
+            : (
+                currentSessionId ?
+                <GameSession
+                    sessionId={currentSessionId}
+                    playerNickname={nickname}
+                />
+                :
+                <div className={styles.enteringSceneContainer}>
+                    <img src={tichuLogo} alt={"Tichu Logo"} className={styles.gameLogo}/>
                     <div style={{
                         display: 'flex',
                         flexDirection: 'row',
                         alignItems: 'center',
-                        justifyContent: 'space-around',
+                        columnGap: '1ch',
                     }}>
-                        <button onClick={onCreate}>Create New Room</button>
-                        <button onClick={onJoin}>Join Open Room</button>
+                        <span>Nickname:</span>
+                        <input
+                            value={nickname}
+                            onChange={e => setNickname(e.target.value)}
+                        />
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'start',
+                        rowGap: '1ch',
+                    }}>
+                        <WinScoreSelector onSelected={onWinningScoreSelected}/>
+                        <div style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: 'space-around',
+                        }}>
+                            <button onClick={onCreate}>Create New Room</button>
+                            <button onClick={onJoin}>Join Open Room</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
-    }</>;
+            )
+        }</div>
+    );
 }
+
+export default App;
